97118ffaf2702c80c0373672425a2e99998a08bd
Gyunseo Lee <gyunseo.lee@gmail.com>
2025-05-24 02:30:56 +0900

    hotfix(be): allow PATCH method

--- backend/src/main/java/com/skkutable/config/WebConfig.java
+++ backend/src/main/java/com/skkutable/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("*")
-        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
         .allowedHeaders("Authorization", "Content-Type")
         .exposedHeaders("*")
         .maxAge(3600);
