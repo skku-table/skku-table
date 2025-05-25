32f2f66b917266a8cd2b3dfd36be433865e5f5a8
Gyunseo Lee <gyunseo.lee@gmail.com>
2025-05-25 23:17:25 +0900

    feat(be): return user id

--- backend/src/main/java/com/skkutable/dto/UserDto.java
+++ backend/src/main/java/com/skkutable/dto/UserDto.java
@@ -9,6 +9,8 @@ import lombok.Getter;
 
 @Getter
 public class UserDto {
+  private final Long id;
+
   @NotBlank(message = "이름을 입력해야 합니다.")
   @Pattern(regexp = "^[가-힣A-Za-z0-9]+$", message = "보관함 이름에는 한글, 영문, 숫자만 가능합니다")
   @Size(min = 1, max = 5, message = "이름은 한글자 이상, 다섯글자 이하입니다.")
@@ -24,7 +26,9 @@ public class UserDto {
 
   private final Role role;
 
-  public UserDto(String name, String email, String password, Role role) {
+
+  public UserDto(Long id, String name, String email, String password, Role role) {
+    this.id = id;
     this.name = name;
     this.email = email;
     this.password = password;
