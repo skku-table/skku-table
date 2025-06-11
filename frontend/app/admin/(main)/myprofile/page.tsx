3103f1d0630e8de7ae7aae777a25f6a18df44485
nuyh2 <ally224.naver@gmail.com>
2025-06-11 11:50:23 +0900

    feat: extract shared component for profile edit page used by both client and admin

--- frontend/app/admin/(main)/myprofile/page.tsx
+++ frontend/app/admin/(main)/myprofile/page.tsx
@@ -1,8 +1,2 @@
-export default function ProfilePage() {
-    return (
-      <div>
-        <h1>프로필</h1>
-        <p>프로필 페이지입니다!</p>
-      </div>
-    );
-  }
+import MyProfilePage from '@/components/MyProfilePage';
+export default MyProfilePage;
